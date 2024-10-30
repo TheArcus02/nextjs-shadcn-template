e0c5d5b51aee2ca59139d57189b84e984f35af2b
TheArcus02 <m.urbaniak.inf@gmail.com>
2024-10-30 13:02:36 +0100

    eslint setup, fonts, metadata

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
-          name: 'next-app',
-          url: undefined,
-          ogImage: undefined,
-          description: 'Lorem ipsum',
-          links: {},
+  name: 'next-app',
+  url: '',
+  ogImage: '',
+  description: 'Lorem ipsum',
+  links: {},
 };
---
+++ lib/fonts.ts
@@ -0,0 +1,11 @@
+import localFont from 'next/font/local'
+
+export const geistSans = localFont({
+  src: '../assets/fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+})
+
+export const geistMono = localFont({
+  src: '../assets/fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+})
